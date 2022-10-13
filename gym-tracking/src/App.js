9c833d898650865c5ac09cfab60e38dda6465985
idbic <dbicga@gmail.com>
2022-10-13 14:26:28 -0700

    making sure that my changes are commited with the to do list

---
+++ gym-tracking/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Your moms fitness app. </h1>
+      <h6>a weird fetish site that actually works as a gym tracking app for some reason. </h6>
+      <p>
+        To Do:
+        <br />-create a gym app that tracks exercises and food/calorie intake in macronutrient
+        metrics.
+        <br />-first lets go ahead and write the javascript logic to allow the form inputs to be totalled to the overall and log the individual data deposit. 
+        <br />-there will be two seperate user input forms, one for tracking exercises, one for tracking food in the forms of calories total, calories in meal, itemized calorie totals, each assigned macro nutrient total.
+        <br />-start with the food form as that will most likely be more difficult. 
+        <br />Food form fields:
+        <br />Food Name.
+        <br />Total Calories. 
+        <br />field for protien, carb, fat
+        <br />Ability to store option so that you do not need to fill out the form again for this meal as it is already an option. 
+        <br />Your mom will be involved in some way shape or form 
+      </p>
+    </div>
+  );
+}
+
+export default App;
